ad93a32e94bd06eaaf1cc98c7f908335238b9430
Yehoshua Levin <ylevin.kw@gmail.com>
2018-06-18 21:09:46 -0400

    replace is really a new string because strings are immutable

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string y = "Yehoshua";
+            y.Replace("Y", "Z");
+            Console.WriteLine(y);// will still output Yehoshua because strings are immutable so what is really happening 
+                                 // by replace is a new string is being created but not catching that new string and printint out the old string
+            Console.ReadKey(true);
+
+            string a = "Yehoshua";
+            a = a.Replace("Y", "Z");
+            Console.WriteLine(a);
+            Console.ReadKey(true);
+        }
+    }
+}
